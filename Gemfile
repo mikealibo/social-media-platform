1068a35ef67b8c21efc639516760d56f7a60ca00
Mike <miksalibo@gmail.com>
2023-09-30 11:29:56 +0800

    added rolify

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ gem "haml-rails", "~> 2.0"
 gem 'html2haml'
 
 gem "devise"
+gem "rolify"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
+  rolify
   # Include default devise modules. Others available are:
   # :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :confirmable, :lockable, :timeoutable, :trackable
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:user) if self.roles.blank?
+  end
 end
